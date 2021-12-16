2c609329a2500dd5703183655c3b949f31392339
Vladimir <vladimir.golovkin16@yandex.ru>
2021-12-16 14:01:09 +0500

    add: AlertRoute

---
+++ Sources/MintRouter/Contracts/BaseCoordinatorRoutable.swift
@@ -0,0 +1,12 @@
+//
+//  BaseCoordinatorRoutable.swift
+//  
+//
+//  Created by Vladimir Golovkin on 16.12.2021.
+//
+
+import UIKit
+
+public protocol BaseCoordinatorRoutable {
+    var controller: UIViewController { get }
+}
